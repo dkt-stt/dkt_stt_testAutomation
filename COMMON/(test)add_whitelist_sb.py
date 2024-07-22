0f98321f9a73a6a5d013a4eb0d30a2a00a1140d8
andy.sc <131955574+123qsdfg@users.noreply.github.com>
2024-07-22 13:34:22 +0900

    샌드박스 화이트리스트 자동 등록

--- COMMON/(test)add_whitelist_sb.py
+++ COMMON/(test)add_whitelist_sb.py
@@ -43,7 +43,7 @@ def login(page):
 def main():
     with sync_playwright() as p:
         # 브라우저 생성
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=False, slow_mo=1000)
         context = browser.new_context()
         page = context.new_page()
 
@@ -53,54 +53,23 @@ def main():
 
         # 반복 작업
         while True:
-            # 사내 번호 관리 진입
-            page.click("text=SMS 발송내역 조회")
-
+            # 화이트리스트 관리 메뉴 이동 
+            page.get_by_role("link", name="Tools").click()
+            page.get_by_role("link", name="통합 화이트리스트 관리").click()
+            # 신규 등록 메뉴 이동
+            page.get_by_role("link", name="신규 등록").click()
             # 전화번호 입력
-            phonenumber = input('인증번호를 요청한 전화번호를 입력해 주세요 : ')
-
+            email = input("화이트 리스트 등록할 아이디 혹은 전화번호를 입력해주세요 ")
             # 전화번호 입력창에 입력
-            page.fill('input[name="phone_number"]', phonenumber)
-
-            # 체크 클릭
-            page.click('input[name="commit"]')
-
-            # 1초 쉼
-            time.sleep(1)
-
-            # 해당 문구가 노출된 경우(번호 잘못 입력한 경우)
-            try:
-                element = page.text_content('//*[@id="sms-pane-search"]/table[2]/tbody/tr[1]/td[5]')
-            except Exception:
-                print("")
-                print("인증번호가 존재하지 않습니다.")
-                retry = input("번호를 다시 입력하시겠습니까? (Y/N 입력): ").strip().upper()
-                print("")
-                # Y 외에 입력 경우
-                if retry != "Y":
-                    print("")
-                    print("5초 후에 프로그램이 자동으로 종료됩니다.")
-                    time.sleep(5)
-                    sys.exit()
-            # 정상적으로 입력한 경우
-            else:
-                print("")
-                # 요청한 번호의 인증코드
-                PhoneCode = page.text_content('//*[@id="sms-pane-search"]/table[2]/tbody/tr[1]/td[5]')
-                print("인증번호 :" + PhoneCode)
-
-                # 인증코드 요청시간
-                PhoneCode_time = page.text_content('//*[@id="sms-pane-search"]/table[2]/tbody/tr[1]/td[6]')
-                print("요청시간 :" + PhoneCode_time)
-                print("")
-
-                retry = input("추가 인증코드가 필요하십니까? (Y/N 입력): ").strip().upper()
-                # Y 외에 입력 경우
-                if retry != "Y":
-                    print("")
-                    print("5초 후에 프로그램이 자동으로 종료됩니다.")
-                    browser.close()
-                    time.sleep(5)
-                    sys.exit()
+            page.get_by_placeholder("이메일 또는 전화번호").fill(email)
+            # 모든 타겟 체크
+            page.locator("#bypass_targets_all").check()
+            # 사유 입력
+            page.get_by_placeholder("등록 사유를 입력해 주세요 (CRMS 등)").fill("테스트용")
+            # 날짜 입력 
+            date = input("만료 날짜를 입력해주세요 ex: YYYY-MM-DD : ")
+            page.get_by_label("만료일").fill(date)
+            # 등록 버튼 선택 
+            page.get_by_role("button", name="등록").click()
 if __name__ == "__main__":
     main()
