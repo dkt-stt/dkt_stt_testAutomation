bc346d9c3d5963d591af542d8ae4ac7b8f81b884
andy.sc <131955574+123qsdfg@users.noreply.github.com>
2024-09-16 23:49:26 +0900

    next_gui_test

---
+++ KAKAOPAGE/exid_next/STTool.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from other_code import execute_code  # other_code.py에서 함수 가져오기
+
+
+# 입력정보 받음
+def on_button_click():
+    user_id = id_entry.get()  # 입력된 ID 값 가져오기
+    user_pw = pw_entry.get()  # 입력된 PW 값 가져오기
+    uid = uid_entry.get()  # 입력된 uid 값 가져오기
+    num = uid_entry.get()  # 입력된 추가횟수 값 가져오기
+    print(f"ID: {user_id}, PW: {user_pw}, UID: {uid}, 추가 횟수: {num}")
+
+    # 여기서 execute_code 함수에 필요한 값을 넘겨줄 수 있습니다.
+    execute_code(user_id, user_pw, uid, int(num))
+
+# Tkinter 창 설정
+window = tk.Tk()
+window.geometry("400x700")
+window.title("스토리테스트기술팀 자동화 툴")
+
+# 창 크기 고정 (크기 조절 불가)
+# window.resizable(False, False)
+
+# PhotoImage를 사용하여 이미지 로드 (PNG 또는 GIF)
+img = tk.PhotoImage(file="cover.png")  # 이미지 파일 경로
+
+# Label 위젯에 이미지 삽입
+label = tk.Label(window, image=img)
+label.pack()
+
+# ID와 PW 입력을 위한 프레임 생성
+input_frame = tk.Frame(window)
+input_frame.pack(pady=10)
+
+# ID 입력을 위한 레이블과 입력창을 한 줄에 배치
+id_label = tk.Label(input_frame, text="ID :")
+id_label.grid(row=0, column=0, padx=5, pady=5)
+id_entry = tk.Entry(input_frame)
+id_entry.grid(row=0, column=1, padx=5, pady=5)
+
+# PW 입력을 위한 레이블과 입력창을 한 줄에 배치
+pw_label = tk.Label(input_frame, text="PW :")
+pw_label.grid(row=1, column=0, padx=5, pady=5)
+pw_entry = tk.Entry(input_frame, show="*")  # 비밀번호는 *로 표시
+pw_entry.grid(row=1, column=1, padx=5, pady=5)
+
+# UID 입력을 위한 레이블과 입력창을 한 줄에 배치
+uid_label = tk.Label(input_frame, text="추가할 계정의 UID :")
+uid_label.grid(row=2, column=0, padx=5, pady=5)
+uid_entry = tk.Entry(input_frame)
+uid_entry.grid(row=2, column=1, padx=5, pady=5)
+
+# 기기 제한 횟수 입력을 위한 레이블과 입력창
+num_label = tk.Label(input_frame, text="추가 횟수 :")
+num_label.grid(row=3, column=0, padx=5, pady=5)
+num_entry = tk.Entry(input_frame)
+num_entry.grid(row=3, column=1, padx=5, pady=5)
+
+# 적용 버튼 생성
+start_button = tk.Button(window, text="기기제한 늘리기", command=on_button_click)
+start_button.pack(pady=3)
+
+# 종료 버튼 생성
+exit_button = tk.Button(window, text="종료", command=window.quit)
+exit_button.pack(pady=3)
+
+# Tkinter 루프 실행
+window.mainloop()
---
+++ KAKAOPAGE/exid_next/other_code.py
@@ -0,0 +1,44 @@
+# other_code.py
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def execute_code(user_id, user_pw, uid, add):
+    # Selenium 웹 드라이버 설정
+    driver = webdriver.Chrome()
+
+    # 암묵적 대기 설정 (페이지 로딩 기다림)
+    driver.implicitly_wait(10)
+
+    # exid 이동
+    driver.get("https://sandbox-charlie.kakaopage.com/exid/login/?referer=/exid/")
+
+    # 계정정보 입력 후 로그인
+    driver.find_element(By.CSS_SELECTOR, '#user_id').send_keys(user_id)
+    driver.find_element(By.CSS_SELECTOR, '#user_pw').send_keys(user_pw)
+    driver.find_element(By.CSS_SELECTOR, '#loginForm > div:nth-child(5) > div > button').click()
+
+    # 고객 센터 이동 후 uid 입력
+    driver.find_element(By.XPATH, "//div[text()='고객 센터']").click()
+    driver.find_element(By.XPATH, "//div[text()='고객 조회']").click()
+    driver.find_element(By.CSS_SELECTOR, 'input[aria-label="카카오페이지 UID:"]').send_keys(uid)
+
+    # 새 창 띄우기
+    main_window = driver.current_window_handle  # 메인 윈도우 핸들 저장
+    driver.find_element(By.XPATH, "//button[text()='등록 기기 관리']").click()
+
+    # 새로 열린 창으로 전환
+    WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))  # 새로운 창이 열릴 때까지 대기
+    new_window = [window for window in driver.window_handles if window != main_window][0]
+    driver.switch_to.window(new_window)
+
+    # addnum 횟수만큼 반복
+    for i in range(add):
+        driver.find_element(By.XPATH, "//button[text()='횟수부여']").click()
+        driver.find_element(By.XPATH, "//button[text()='부여']").click()
+
+    print("f{add}회 추가 완료")
+
+    # 브라우저 종료
+    driver.quit()
